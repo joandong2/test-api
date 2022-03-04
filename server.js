c6fc87ac29d2590340401d06059f3e4378053f8f
John Oblenda <joandong2@gmail.com>
2022-03-04 13:35:15 -0500

    cors issue

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 // Routes
 const postsRoutes = require("./routes/api/posts");
 
 const app = express();
+app.use(cors());
 app.use(express.json()); //body
 
 // Connect to mongoDB
